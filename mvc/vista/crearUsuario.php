29d039f343d9e5fb15716b357deaa57da4709c75
darkonwar <darkonwar@gmail.com>
2017-07-03 21:15:02 -0300

    d
    d

--- mvc/vista/crearUsuario.php
+++ mvc/vista/crearUsuario.php
@@ -1,6 +1,27 @@
-<?php 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>SIS.GES.</title>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="../../CSS/style.css">
+</head>
 
-include_once("../controlador/usuario_controlador.php");
+<body>
+	<?php
+	//Sesion
+    $usuario="";
+    $idUsuario=1;
+        session_start();
+        if (!isset($_SESSION['id'])){
+            header ("Location:../index.html");
+        } else {
+            $usuario = $_SESSION['nom']." ".$_SESSION['ape'];
+            $idUsuario = $_SESSION['id'];
+        }
+		//Determina dia actual
+		$today = date( "d/m/Y", mktime());
+		//Formulario
+	include_once("../controlador/usuario_controlador.php");
 
 	$controlador = new controladorUsuario();
 	if(isset($_POST['enviar'])){
@@ -11,46 +32,87 @@ include_once("../controlador/usuario_controlador.php");
 			echo"Ya existe";
 		}
 	}
-
-
- ?>
- <br>
- <br>
- 
-
-<h3>Crear un nuevo usuario</h3>
-<hr>
-<form id="frmCrear" action="" method="post">
-	<input type="text" placeholder="Nombre" name="UsuNombre">
-	<br />
-	<input type="text" placeholder="Apellido" name="UsuApellido" requrired>
-	<br />
-	<input type="text" placeholder="DNI" name="UsuDNI" requrired>
-	<br />
-	<input type="text" placeholder="Direccion" name="UsuDireccion" requrired>
-	<br />
-	<input type="text" placeholder="Telefono" name="UsuTelefono" requrired>
-	<br />
-	<input type="text" placeholder="Mail" name="UsuMail" requrired>
-	<br />
-	<input type="text" placeholder="Fecha Nacimiento" name="UsuFechaNacimiento" requrired>
-	<br />
-	<input type="text" placeholder="Localidad Opera" name="UsuLocalidadOpera" requrired>
-	<br />
-    <input type="text" placeholder="Cuenta Usuario" name="UsuCuenta" requrired>
-	<br />
-   	<input type="text" placeholder="Password Usuario" name="UsuPassword" requrired>
-	<br />
-    <input type="text" placeholder="Fecha Ingreso" name="UsuFechaIngreso" requrired>
-	<br />
-	<input type="text" placeholder="Fecha Egreso" name="UsuFechaEgreso" requrired>
-	<br />
-	<input type="text" placeholder="Estado" name="UsuEstado" requrired>
-	<br />
-    <input id="button" type="button" onClick="document.getElementById('frmCrear').submit()" value="Crear">
-
-		<ul>
-			<li><a href="index.php">Inicio</a></li>
-		</ul>
-
-</form>
+    ?>
+	<div class="background">
+    </div>
+    <div class="header">
+        <header>
+        	<table width="100%">
+                <tr>
+                    <td width="25%" align="left"><img src="../../Images/GrupoAcademico.jpg" width="638" height="633" style="width:100px;height:100px;"></td>
+                    <td width="50%">Logueado como: <?php echo $usuario?><br>Rol: Administrador</td>
+                    <td width="25%">
+                    <form id="frmLogin" action="../../Login PHP/logout.php" method="post">
+                        <input name="return" type="hidden" value="<?php echo urlencode($_SERVER["PHP_SELF"]);?>" />
+                        <input id="button" type="button" onClick="document.getElementById('frmLogin').submit()" value="Sign Out"/>
+                    </form>
+                    </td>
+                </tr>
+        	</table>
+        </header>
+    </div>
+	<div class="wrap">
+		<nav>
+			<ul class="menu">
+				<li><a href="../../Post_Inicio/sesionAdmin.php"><span class="iconic home"></span> Home</a></li>
+				<li><a href="../../SelectUserOperations/ABMUsuario.php"><span class="iconic pencil-alt"></span><span class="iconic user"></span> ABM Usuarios</a></li>
+                <li><a href="../../UnderConstruction/UnderConstrAdmin.php"><span class="iconic pencil-alt"></span><span class="iconic box"></span> ABM Productos</a></li>
+			</ul>
+			<div class="clearfix"></div>
+        </nav>
+	</div>
+    <div class="formularios">
+    <table width="100%">
+		<tr>
+			<td width="40%">
+            <form id="frmCrear" action="" method="post">
+                <input type="text" placeholder="Nombre" name="UsuNombre">
+                <br />
+                <input type="text" placeholder="Apellido" name="UsuApellido" requrired>
+                <br />
+                <input type="text" placeholder="DNI" name="UsuDNI" requrired>
+                <br />
+                <input type="text" placeholder="Direccion" name="UsuDireccion" requrired>
+                <br />
+                <input type="text" placeholder="Telefono" name="UsuTelefono" requrired>
+                <br />
+                </form>
+            </td>
+            <td width="20%">
+            </td>
+            <td width="40%">
+                <form>
+                <input type="text" placeholder="Mail" name="UsuMail" requrired>
+                <br />
+                <input type="text" placeholder="Fecha Nacimiento" name="UsuFechaNacimiento" requrired>
+                <br />
+                <input type="text" placeholder="Localidad Opera" name="UsuLocalidadOpera" requrired>
+                <br />
+                <input type="text" placeholder="Cuenta Usuario" name="UsuCuenta" requrired>
+                <br />
+                <input type="text" placeholder="Password Usuario" name="UsuPassword" requrired>
+                <input type="hidden" value="<?php $today?>" name="UsuFechaIngreso" requrired>
+                <input type="hidden" value="<?php ''?>" name="UsuFechaEgreso" requrired>
+                <input type="hidden" value="<?php 'activo'?>" name="UsuEstado" requrired>
+                <br />
+                </form>
+            </td>
+		</tr>
+        <tr>
+            <td width="25%">
+            </td>
+        	<td width="50%">
+            	<form>
+                <input id="button" type="button" onClick="document.getElementById('frmCrear').submit()" value="Crear">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                </ul>
+            </form>
+            </td>
+            <td width="25%">
+            </td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
