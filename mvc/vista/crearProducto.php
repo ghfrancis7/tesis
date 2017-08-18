923848867d682bb724bf6969543200f945a7bac1
darkonwar <darkonwar@gmail.com>
2017-08-18 16:07:24 -0300

    aa

--- mvc/vista/crearProducto.php
+++ mvc/vista/crearProducto.php
@@ -45,7 +45,7 @@
 			<ul class="menu">
 				<li><a href="../../Post_Inicio/sesionAdmin.php"><span class="iconic home"></span> Home</a></li>
 				<li><a href="../../SelectUserOperations/ABMUsuario.php"><span class="iconic pencil-alt"></span><span class="iconic user"></span> ABM Usuarios</a></li>
-                <li><a href="../../mvc/vista/crearProducto.php"><span class="iconic pencil-alt"></span><span class="iconic box"></span> ABM Productos</a></li>
+                <li><a href="../../SelectUserOperations/ABMProducto.php"><span class="iconic pencil-alt"></span><span class="iconic box"></span> ABM Productos</a></li>
 			</ul>
 			<div class="clearfix"></div>
         </nav>
