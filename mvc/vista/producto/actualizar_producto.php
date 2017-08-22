0a04a6bfe871e8462b59d81d9dde12ab57403a0c
ghfrancis7 <ghfrancis7@gmail.com>
2017-08-22 20:07:53 -0300

    Cambios en editar producto

--- mvc/vista/producto/actualizar_producto.php
+++ mvc/vista/producto/actualizar_producto.php
@@ -5,20 +5,16 @@
 			$IDProducto = $_POST['IDProducto'];
 			$ProductoNombre = $_POST['ProductoNombre'];
 			$ProductoPrecio = $_POST['ProductoPrecio'];
-			$ProductoFechaAltaDB = $_POST['ProductoFechaAltaDB'];
-			$ProductoFechaBajaDB = $_POST['ProductoFechaBajaDB'];
-			$ProductoEstado = $_POST['ProductoEstado'];
+			$ProductoNumeroSerie = $_POST['ProductoNumeroSerie'];
 
 		try {
 
 			$pdo->mysql->beginTransaction();
-			$pst = $pdo->mysql->prepare("UPDATE producto set ProductoNombre = :ProductoNombre, ProductoPrecio = :ProductoPrecio, ProductoFechaAltaDB =:ProductoFechaAltaDB, ProductoFechaBajaDB =:ProductoFechaBajaDB, ProductoEstado =:ProductoEstado where IDProducto = :IDProducto");
+			$pst = $pdo->mysql->prepare("UPDATE producto set ProductoNombre = :ProductoNombre, ProductoPrecio = :ProductoPrecio, ProductoNumeroSerie =:ProductoNumeroSerie where IDProducto = :IDProducto");
 			$pst->bindParam(":IDProducto",$IDProducto,PDO::PARAM_STR);
 			$pst->bindParam(":ProductoNombre",$ProductoNombre,PDO::PARAM_STR);
 			$pst->bindParam(":ProductoPrecio",$ProductoPrecio,PDO::PARAM_STR);
-			$pst->bindParam(":ProductoFechaAltaDB",$ProductoFechaAltaDB,PDO::PARAM_STR);
-			$pst->bindParam(":ProductoFechaBajaDB",$ProductoFechaBajaDB,PDO::PARAM_STR);
-			$pst->bindParam(":ProductoEstado",$ProductoEstado,PDO::PARAM_STR);
+			$pst->bindParam(":ProductoNumeroSerie",$ProductoNumeroSerie,PDO::PARAM_STR);
 			
 			$pst->execute();
 
--- mvc/vista/producto/editar_producto.php
+++ mvc/vista/producto/editar_producto.php
@@ -6,49 +6,42 @@
 <body>
 
 
-<form method="post" action="actualizar_producto.php">
-<?php 
-	require("../../modelo/Conexion.php");
-	$pdo= new Conexion();
+	<form method="post" action="actualizar_producto.php">
+	<?php 
+		require("../../modelo/Conexion.php");
+		$pdo= new Conexion();
 
-		$IDProducto = $_GET['IDProducto'];
-		$datosProducto = $pdo->mysql->prepare("SELECT * FROM producto where IDProducto = :IDProducto");
-		$datosProducto->bindParam(":IDProducto", $IDProducto, PDO::PARAM_INT);
-		$datosProducto->execute();
-		$producto = $datosProducto->fetch();
+			$IDProducto = $_GET['IDProducto'];
+			$datosProducto = $pdo->mysql->prepare("SELECT * FROM producto where IDProducto = :IDProducto");
+			$datosProducto->bindParam(":IDProducto", $IDProducto, PDO::PARAM_INT);
+			$datosProducto->execute();
+			$producto = $datosProducto->fetch();
 
  ?>
 
-<p>
-<label>ID</label>
-<input type="text" name="IDProducto" value="<?php echo $IDProducto ?>" readonly=true >
-</p>
-<p>
-	<label>ProductoNombre</label>
-	<input type="text" name="ProductoNombre" required="true" value="<?php echo $producto['ProductoNombre']; ?>">
-</p>
-<p>
-	<label>ProductoPrecio</label>
-	<input type="text" name="ProductoPrecio" required="true" value="<?php echo $producto['ProductoPrecio']; ?>">
-</p>
-<p>
-	<label>ProductoFechaAltaDB</label>
-	<input type="text" name="ProductoFechaAltaDB" required="true" placeholder="Fecha de Alta" value="<?php echo $producto['ProductoFechaAltaDB']; ?>">
-</p>
-<p>
-	<label>ProductoFechaBajaDB</label>
-	<input type="text" name="ProductoFechaBajaDB" placeholder="Fecha de Baja" value="<?php echo $producto['ProductoFechaBajaDB']; ?>">
-</p>
-<p>
-	<label>ProductoEstado</label>
-	<input type="text" name="ProductoEstado" required="true" placeholder="Estado" value="<?php echo $producto['ProductoEstado']; ?>">
-</p>
+	<p>
+		<label>ID</label>
+		<input type="text" name="IDProducto" value="<?php echo $IDProducto ?>" readonly=true >
+	</p>
+	<p>
+		<label>Producto Numero Serie</label>
+		<input type="text" name="ProductoNumeroSerie" required="true" placeholder="Numero Serie Producto" value="<?php echo $producto['ProductoNumeroSerie']; ?>">
+	</p>
+	<p>
+		<label>ProductoNombre</label>
+		<input type="text" name="ProductoNombre" required="true" value="<?php echo $producto['ProductoNombre']; ?>">
+	</p>
+	<p>
+		<label>ProductoPrecio</label>
+		<input type="text" name="ProductoPrecio" required="true" value="<?php echo $producto['ProductoPrecio']; ?>">
+	</p>
 
-<input type="submit" name="modificar" value="Modificar">
- <input type="button" value="Cancelar" onclick="history.back(-1)" />
+
+	<input type="submit" name="modificar" value="Modificar">
+ 	<input type="button" value="Cancelar" onclick="history.back(-1)" />
 
 
 
-</body>
-</html>
+		</body>
+	</html>
 </form>
