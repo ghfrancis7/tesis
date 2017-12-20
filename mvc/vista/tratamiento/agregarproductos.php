2ddb6373986f0b11ca72252b7e632615ff2b9510
ghfrancis7 <ghfrancis7@gmail.com>
2017-12-19 21:39:18 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- mvc/vista/tratamiento/agregarproductos.php
+++ mvc/vista/tratamiento/agregarproductos.php
@@ -96,7 +96,7 @@
                 </div>
             </td>
             <td width="40%">
-                <input type="text" style="width:75%" name="LoteCantidad">
+                <input type="text" style="width:75%" name="LoteCantidad" onkeypress="return valida(event)">
             </td>
             <td width="30%">
                 <input type="hidden" name="IDTratamiento" value= <?php echo $IDTratamiento ?> />
@@ -137,4 +137,19 @@
 	</table>
 	</div>
 </body>
+<script>
+    function valida(e){
+        tecla = (document.all) ? e.keyCode : e.which;
+    
+        //Tecla de retroceso para borrar, siempre la permite
+        if (tecla==8){
+            return true;
+        }
+            
+        // Patron de entrada, en este caso solo acepta numeros
+        patron =/[0-9]/;
+        tecla_final = String.fromCharCode(tecla);
+        return patron.test(tecla_final);
+    }
+</script>
 </html>
